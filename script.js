dfba7c085965c2cd8fa5abd791562ab2b7265849
siddharthbaleja7 <144931848+siddharthbaleja7@users.noreply.github.com>
2024-02-03 18:22:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var post = document.getElementById('button');
+var type = document.getElementById('p');
+var column = document.getElementsByClassName('col2');
+var none = document.querySelector('.noness');
+var deleteconfirm = document.querySelector('.delete-confirm')
+
+var deletes = document.getElementById('delete');
+deletes.addEventListener('click',()=>{
+    none.style.display = "none";
+})
+deletes.addEventListener('click',()=>{
+    window.alert(
+        console.log("Are you confirm")
+)
+
+})
+
+
+
+
+        
+    
+
